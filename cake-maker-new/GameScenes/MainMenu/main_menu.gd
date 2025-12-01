f62efd02de8aa2083d8823c5a335c965f53dee7b
Ayella Pausal <pausalayella2@gmail.com>
2025-12-02 00:22:09 +0800

    Imported other folder for comparison

--- cake-maker-new/GameScenes/MainMenu/main_menu.gd
+++ cake-maker-new/GameScenes/MainMenu/main_menu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@onready var press: AudioStreamPlayer2D = $Buttons_Main/Quit/Press
 var Bgm_bus = AudioServer.get_bus_index("Bgm_bus")
 
 var button_type = null
@@ -9,7 +8,7 @@ func _ready() -> void:
 	MusicPlayer.playMusic()
 
 func pressButton() -> void:
-	press.play()
+	SFX.Press()
 
 func _on_start_pressed() -> void:
 	button_type = "start"
@@ -17,17 +16,17 @@ func _on_start_pressed() -> void:
 	$FadeTransition.show()
 	$FadeTransition/FadeTimer.start()
 	$FadeTransition/AnimationPlayer.play("FadeIn")
-	pressButton()
+	SFX.Press()
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	pressButton()
+	SFX.Press()
 
 
 func _on_mute_bgm_pressed() -> void:
 	AudioServer.set_bus_mute(Bgm_bus, not AudioServer.is_bus_mute(Bgm_bus))
-	pressButton()
+	SFX.Press()
 
 
 func _on_fade_timer_timeout() -> void:
